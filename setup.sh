b0bede77adeb33adf67bb39318dbb59f4b0d1680
lazyoracle <contact@anuragsaharoy.me>
2020-10-25 19:14:18 +0530

    fix broken user prompt, missing openblas, add-apt-repository

--- setup.sh
+++ setup.sh
@@ -1,20 +1,16 @@
 echo "This script is not idempotent. It is to be run only once at setup"
 echo "Things are sure to break if this is run a second time"
-read -p "Press enter if you understand and wish to continue"
 
 echo "Update the package list..."
 sudo apt update
 echo "Upgrade installed packages..."
-read -p "Press enter to continue"
 sudo apt upgrade -y
 echo "Install necessary packages to minimal Ubuntu system..."
-read -p "Press enter to continue"
-sudo apt install -y fish wget bzip2 curl git gcc g++ python3-dev build-essential vim nano rsync htop tree screen libatlas-base-dev libboost-all-dev
+sudo apt install -y fish wget bzip2 curl git gcc g++ python3-dev build-essential vim nano rsync htop tree screen libatlas-base-dev libboost-all-dev libopenblas-dev
 sudo apt clean
 sudo apt autoremove -y
 
 echo "Install miniconda..."
-read -p "Press enter to continue"
 wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b -p $HOME/miniconda3
@@ -28,12 +24,10 @@ source ~/.bashrc
 rm miniconda.sh
 
 echo "Update Miniconda"
-read -p "Press enter to continue"
 conda update conda -q -y
 conda clean -a -y -q
 
 echo "Add a swap file"
-read -p "Press enter to continue"
 echo "Enter swap value in GB as such: 10G for 10 GB swap area"
 read swapvalue
 sudo fallocate -l $swapvalue /swapfile
@@ -48,7 +42,7 @@ echo "/swapfile swap swap defaults 0 0"
 
 
 echo "Installing GPU Drivers"
-read -p "Press enter to continue"
+sudo apt-get install software-properties-common
 curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
