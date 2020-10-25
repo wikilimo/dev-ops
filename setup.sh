a24b7cbe92f22cacc1d55f6aca5191fc6f9e0454
lazyoracle <contact@anuragsaharoy.me>
2020-10-25 18:49:52 +0530

    add gpu drivers for gcp

--- setup.sh
+++ setup.sh
@@ -47,5 +47,14 @@ echo "Add the following at the end of /etc/fstab"
 echo "/swapfile swap swap defaults 0 0"
 
 
+echo "Installing GPU Drivers"
+read -p "Press enter to continue"
+curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+sudo apt update
+sudo apt install -y cuda
+
 echo "Rebooting system in 5s. Please login again after some time..."
 sleep 5; sudo reboot -h now
